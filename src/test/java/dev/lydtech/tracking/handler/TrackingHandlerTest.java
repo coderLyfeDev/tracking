312fdbccbbfaccbcc1ffec39109dd887ad332cb3
brandonharshaw <brandonharshaw24@gmail.com>
2024-06-14 07:05:09 -0700

    Added unit testing

---
+++ src/test/java/dev/lydtech/tracking/handler/TrackingHandlerTest.java
@@ -0,0 +1,38 @@
+package dev.lydtech.tracking.handler;
+
+import dev.lydtech.tracking.message.DispatchPrepared;
+import dev.lydtech.tracking.service.TrackingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static java.util.UUID.randomUUID;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class TrackingHandlerTest {
+
+    private TrackingHandler handler;
+    private TrackingService trackingServiceMock;
+
+
+    @BeforeEach
+    void setUp() {
+        trackingServiceMock = mock(TrackingService.class);
+        handler = new TrackingHandler(trackingServiceMock);
+    }
+
+    @Test
+    void listen_success() throws Exception{
+        DispatchPrepared dispatchPrepared = new DispatchPrepared(randomUUID());
+        handler.listen(dispatchPrepared);
+        verify(trackingServiceMock, times(1)).process(dispatchPrepared);
+    }
+
+    @Test
+    void listen_throwsException() throws Exception{
+        DispatchPrepared dispatchPrepared = new DispatchPrepared(randomUUID());
+        doThrow(new RuntimeException("process failed")).when(trackingServiceMock).process(dispatchPrepared);
+        handler.listen(dispatchPrepared);
+        verify(trackingServiceMock, times(1)).process(dispatchPrepared);
+    }
+}
---
+++ src/test/java/dev/lydtech/tracking/service/TrackingServiceTest.java
@@ -0,0 +1,17 @@
+package dev.lydtech.tracking.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrackingServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void process() {
+    }
+}
